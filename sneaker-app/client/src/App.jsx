0b57c27dac76fb07a24aa71efdf543157c9fdc94
John Paul Junglen <j.junglen@gmail.com>
2026-05-19 09:04:37 -0500

    added the pages imports and Routes

--- sneaker-app/client/src/App.jsx
+++ sneaker-app/client/src/App.jsx
@@ -1,10 +1,47 @@
-import { useState } from 'react'
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import Navbar from "../components/Navbar";
+import Register from "../pages/Register";
+import Login from "../pages/Login";
+import Profile from "../pages/Profile";
+import Collection from "../pages/Collection"
+import Landing from "../pages/Landing"
 import './index.css'
+
+function ProtectedRoute({ children }) {
+  const { user, loading } = useAuth();
+
+  if (loading) return null
+  
+  if (!user) {
+    return <Navigate to="/login" replace />
+
+  }
+
+  return children
+
+}
 
 function App() {
   return (
-    <div className="min-h-screen bg-slate-900 text-white flex items-center justify-center">
-      <h1 className="text-3xl font-bold text-blue-900">The Laboratory</h1>
+    <div className="min-h-screen bg-stone-300">
+      <Navbar/>
+      <Routes>
+        {/* Public routes */}
+        <Route path="/" element={<Landing />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        {/* Protected routes */}
+        <Route path="/collection" element={
+          <ProtectedRoute>
+            <Collection />
+          </ProtectedRoute>} />
+
+        <Route path="/profile" element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>} />
+      </Routes>
     </div>
   );
 }
